271fa5cf89873f017f9ee80df585a1d32f74eb0f
nixon <nixon@LAMAR>
2022-06-07 21:23:19 -0600

    Agregando Entity Framework Code First

---
+++ AutoDealers/AutoDealers.BL/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDealers.BL
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public double Precio { get; set; }
+        public int Existencia { get; set; }
+    }
+}
